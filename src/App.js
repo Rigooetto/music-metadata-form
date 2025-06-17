010227eecd253a7861fd960f44deed1973253a6a
Ivan Sandoval <ivan@rigolettoprostudio.com>
2025-06-16 21:51:47 -0700

    Reverted to WAS

--- src/App.js
+++ src/App.js
@@ -2367,7 +2367,6 @@ const removeComposer = (trackIndex, composerIndex) => {
 </div>                     
 
 
-
                       
                     </div>
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
