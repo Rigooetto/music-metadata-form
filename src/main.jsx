e8f0b4a7bb0dcff94e45b8f1b7d598796b15eb89
Ivan Sandoval <ivan@rigolettoprostudio.com>
2025-06-23 23:57:38 -0700

    seguimos con los fonts chale

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import '@fontsource/inter/variable.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
